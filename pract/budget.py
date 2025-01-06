ad5f3b560893997ed626d4b20c999d9dda2c3e3c
Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-05 21:58:41 -0800

    Budget program function practice complete

--- pract/budget.py
+++ pract/budget.py
@@ -5,13 +5,26 @@
 # Outputs the updated remaining budget after deducting the expense.
 # For example, if your starting budget is $3500.75 and you spend $9 on a shirt, the program should display the new remaining budget after the expense. Use functions to organize your code for displaying the budget and calculating the updated amount.
 
+# Target Output:
+# Your remaining budget is: $3500.75
+# Your remaining budget is: $3491.75
+
 # Define global variables
 current_budget = 3500.75
 shirt_expense = 9
 
+def print_current_budget():
+    print(f"The current budget is ${current_budget}")
+
 # Create function to calculate the updated budget remaining after expense:
 
-def budget_expense(budget, expense):
+def deduct_budget(budget, expense):
     return budget - expense
 
+
+remaining_budget = deduct_budget(current_budget, shirt_expense)
+
+
+print_current_budget()
+print(f"The remaining budget after shirt purchase is ${remaining_budget}")
 
--- pract/inputN.py
+++ pract/inputN.py
@@ -3,8 +3,7 @@
 # of height n
 
 def print_triangle(n):
-    for num in range(1, n+1):
-        print("*" * num)
+    for i in range(1, n+1):
+        print("*" * i)
 
-
-print_triangle(5)
+print_triangle(6)
