252edc5a8cfd6d3069dec29f5eeae6f6af6c335d
Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-09 11:04:11 -0800

    Pyramid generator complete using a for loop

--- pract/pyramidGenerator.js
+++ pract/pyramidGenerator.js
@@ -1,5 +1,11 @@
+// Create a pyramid using #'s in which each row there is n+1 #'s up to 8
+
 const character = "#"
 const count = 8
 const rows = []
 
+for (let i = 0; i < count; i++){
+    rows.push(character.repeat(i))
+}
 
+console.log(rows.join('\n'))
