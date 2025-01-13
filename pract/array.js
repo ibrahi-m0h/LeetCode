b751dec1cf4286dc17c89bde96c304a700e47474
Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-12 17:29:27 -0800

    Build a gradebook app using functional programming

--- pract/array.js
+++ pract/array.js
@@ -1,6 +1,20 @@
-const numbers = [1,2,3]
-const shifted = numbers.shift()
+// const numbers = [1,2,3]
+// const shifted = numbers.shift()
 
-console.log(numbers)
+// console.log(numbers)
 
-console.log(shifted)
+// console.log(shifted)
+
+
+// Build a gradebook app using functional programming
+
+function getAverage(scores) {
+    let sum = 0
+    for (let i = 0; i < scores.length; i++){
+      sum+=scores[i]
+    }
+    return sum / scores.length
+  }
+  
+  console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+  console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
